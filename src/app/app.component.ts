2ca99552080a4a661718b47416d0ec72a2139551
Eliane Danae Trejo Aguiñaga <elitrejo0219@gmail.com>
2023-11-22 08:05:24 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,25 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'CBDF_EDTA_09';
+  title = 'Ejecicio 09 - Interpolación';
+  nombre = "Eliane Danae Trejo Aguiñaga";
+  edad = 21;
+  semestre = "Séptimo semestre";
+  calificaciones = [8,7,9,10,8];
+
+  calcularPromedio(){
+    let suma = 0; 
+    const lA = this.calificaciones.length;
+    for (let i = 0; i < lA; i++) 
+      suma += this.calificaciones[i];
+    return suma/lA;
+  }
+
+  estadoAlumno(){
+    if(this.calcularPromedio()>=6){
+      return "Aprobado";
+    }else{
+      return "No aprobado";
+    }
+  }
 }
